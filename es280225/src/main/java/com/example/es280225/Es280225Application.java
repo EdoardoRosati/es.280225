f359ab849765ef32d6c8aed8f7201ac727032862
Edoardo Rosati <e.rosati@dstech.it>
2025-02-25 11:24:52 +0100

    Test commit

---
+++ es280225/src/main/java/com/example/es280225/Es280225Application.java
@@ -0,0 +1,13 @@
+package com.example.es280225;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Es280225Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Es280225Application.class, args);
+	}
+
+}
---
+++ es280225/src/test/java/com/example/es280225/Es280225ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.es280225;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Es280225ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
